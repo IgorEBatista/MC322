b76d3e910c062dc7f3dd4e942e59545b133aa737
Igor Batista <igorengbatista@gmail.com>
2023-03-16 15:47:45 -0300

    Fim do primeira parte do desenvolvimento

--- lab02/Cliente.java
+++ lab02/Cliente.java
@@ -1,3 +1,59 @@
 public class Cliente {
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private int idade;
+    private String endereco;
+
+    // Construtor
+    public Cliente (String nome, String cpf, int idade, String dataNascimento, String endereco){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.dataNascimento = dataNascimento;
+        this.idade = idade;
+        this.endereco = endereco;
+    }
+
+    // Getters e setters
+    public String getNome (){
+        return nome;
+    }
+
+    public void setNome (String nome){
+        this.nome = nome;
+    }
+
+    //public String getNome (){
+    //    return nome;
+    //}
+//
+    //public void setNome (String nome){
+    //    this.nome = nome;
+    //}
     
+
+
+    public String getTelefone (){
+        return cpf;
+    }
+
+    public void setTelefone (String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getdataNascimento (){
+        return dataNascimento;
+    }
+
+    public void setdataNascimento (String dataNascimento){
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEndereco (){
+        return endereco;
+    }
+
+    public void setEndereco (String endereco){
+        this.endereco = endereco;
+    }
 }
--- lab02/Seguradora.java
+++ lab02/Seguradora.java
@@ -1,47 +1,47 @@
 public class Seguradora {
-    private String nome ;
-    private String telefone ;
-    private String email ;
-    private String endereco ;
+    private String nome;
+    private String telefone;
+    private String email;
+    private String endereco;
 
     // Construtor
-    public Seguradora ( String nome , String telefone , String email , String endereco ){
-        this.nome = nome ;
-        this.telefone = telefone ;
-        this.email = email ;
-        this.endereco = endereco ;
+    public Seguradora (String nome, String telefone, String email, String endereco){
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
     }
 
     // Getters e setters
     public String getNome (){
-        return nome ;
+        return nome;
     }
 
-    public void setNome ( String nome ){
-        this.nome = nome ;
+    public void setNome (String nome){
+        this.nome = nome;
     }
 
     public String getTelefone (){
-        return telefone ;
+        return telefone;
     }
 
-    public void setTelefone ( String telefone ){
-        this.telefone = telefone ;
+    public void setTelefone (String telefone){
+        this.telefone = telefone;
     }
 
     public String getEmail (){
-        return email ;
+        return email;
     }
 
-    public void setEmail ( String email ){
-        this.email = email ;
+    public void setEmail (String email){
+        this.email = email;
     }
 
     public String getEndereco (){
-        return endereco ;
+        return endereco;
     }
 
-    public void setEndereco ( String endereco ){
-        this.endereco = endereco ;
+    public void setEndereco (String endereco){
+        this.endereco = endereco;
     }
 }
