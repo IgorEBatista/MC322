71736611f8680bc347738de51e694ee7919e23a7
Igor Batista <igorengbatista@gmail.com>
2023-03-23 13:30:22 -0300

    Termino dos getter e setter iniciais

--- lab02/Sinistro.java
+++ lab02/Sinistro.java
@@ -1,3 +1,38 @@
 public class Sinistro {
+
+    private int id;
+    private String data;
+    private String endereco;
+
+    //Construtor
+    public Sinistro (int id, String data, String endereco){
+        this.id = id;
+        this.data = data;
+        this.endereco = endereco;
+    }
+    // Getters e setters
+    public int getid (){
+        return id;
+    }
+
+    public void setid (int id){
+        this.id = id;
+    }
+
+    public String getdata (){
+        return data;
+    }
+
+    public void setdata (String data){
+        this.data = data;
+    }
+
+    public String getendereco (){
+        return endereco;
+    }
+
+    public void setendereco (String endereco){
+        this.endereco = endereco;
+    }
     
 }
